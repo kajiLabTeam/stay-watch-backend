45e2c916cd5a939725da55d987fe9c3a569430e6
happy663 <tatu2425@gmail.com>
2022-06-06 14:24:15 +0900

    同時に滞在した人を表示時間被りのみ

--- backend/go/app/service/user.go
+++ backend/go/app/service/user.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"Stay_watch/model"
+	"Stay_watch/util"
 	"fmt"
 	"log"
 )
@@ -127,4 +128,58 @@ func (UserService) GetUserIDByUUID(uuid string) (int64, error) {
 	return user.ID, nil
 }
 
-//
+//指定されたログリストと同じ時間にいたユーザを取得する
+func (UserService) GetSameTimeUser(logs []model.Log) ([]model.SimultaneousStayUserGetResponse, error) {
+	targetLogs := make([]model.Log, 0)
+	fmt.Println(logs)
+	dates := make([]string, 0)
+	for _, log := range logs {
+		dates = append(dates, log.StartAt[:10])
+		//時間が被るログを取得
+		err := DbEngine.Table("log").Asc("start_at").Where("start_at >= ?", log.StartAt).And("start_at <= ?", log.EndAt).Or(
+			"end_at >= ? and end_at <= ?", log.StartAt, log.EndAt).Or(
+			"start_at <= ? and end_at >= ?", log.StartAt, log.EndAt).And("room_id = ?", log.RoomID).Find(&targetLogs)
+
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		// DbEngine.Table("log").Where("start_at >= ?", log.StartAt).And("start_at <= ?", log.EndAt)
+		// DbEngine.Table("log").Where("end_at >= ?", log.StartAt).And("end_at <= ?", log.EndAt)
+	}
+
+	simultaneousStayUserGetResponses := make([]model.SimultaneousStayUserGetResponse, 0)
+
+	UserService := UserService{}
+
+	utilService := util.Util{}
+	dates = utilService.SliceUniqueString(dates)
+	fmt.Println(dates)
+	for _, date := range dates {
+
+		userIDs := make([]int64, 0)
+		for _, log := range targetLogs {
+			if log.StartAt[:10] == date {
+				userIDs = append(userIDs, log.UserID)
+			}
+		}
+		uniqueUserIDs := utilService.SliceUniqueNumber(userIDs)
+
+		names := make([]model.Name, 0)
+		for _, uniqueUserID := range uniqueUserIDs {
+			userName, err := UserService.GetUserName(uniqueUserID)
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+			names = append(names, model.Name{
+				Name: userName,
+				ID:   uniqueUserID,
+			})
+		}
+
+		simultaneousStayUserGetResponses = append(simultaneousStayUserGetResponses, model.SimultaneousStayUserGetResponse{
+			Date:  date,
+			Names: names,
+		})
+	}
+	return simultaneousStayUserGetResponses, nil
+}
