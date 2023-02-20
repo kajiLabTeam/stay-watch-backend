41c4e68bbb2fb3925c98a28226c1f2d2eb8a66a3
happy663 <tatu2425@gmail.com>
2023-02-21 00:54:28 +0900

    update: 実行する時を月の初めに設定

--- python/app/main.py
+++ python/app/main.py
@@ -10,14 +10,12 @@ def task():
     # 現在の日付を取得
     today = datetime.date.today()
 
-    # 月の最後の日を計算
-    last_day = datetime.date(today.year, today.month,
-                             1) + datetime.timedelta(days=32)
-    last_day = last_day.replace(day=1) - datetime.timedelta(days=1)
+    # 今月の最初の日を計算
+    first_day = datetime.date(today.year, today.month, 1)
 
-    # 今日が月の最後の日かどうかを判定
-    if today == last_day:
-        print("今日は月の最後の日です")
+    # 今日が月の最初の日かどうかを判定
+    if today == first_day:
+        print("今日は月の最初の日です")
         log_times = data.get_log_times()
         print(log_times)
         image_path = pie_chart.save_users_log_time_pie_chart(log_times)
