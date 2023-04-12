85a1c3bab343b76807f76ddee9c3934a73a5d01d
togawa427 <95838999+togawa427@users.noreply.github.com>
2023-04-12 23:10:21 +0900

    merge: mainをfeature-admin-userへマージ

--- go/app/service/beacon.go
+++ go/app/service/beacon.go
@@ -24,3 +24,20 @@ func (BeaconService) GetBeaconByBeaconId(beaconId int64) (model.BeaconType, erro
 
 	return beacon, nil
 }
+
+func (BeaconService) GetBeaconTypeIdByBeaconName(beaconName string) (int64, error) {
+	DbEngine := connect()
+	close, err := DbEngine.DB()
+	if err != nil {
+		return 0, err
+	}
+	defer close.Close()
+	beacon := model.BeaconType{}
+	result := DbEngine.Where("name=?", beaconName).Take(&beacon)
+	if result.Error != nil {
+		fmt.Printf("ユーザ名取得失敗 %v", result.Error)
+		return 0, result.Error
+	}
+
+	return int64(beacon.ID), nil
+}
