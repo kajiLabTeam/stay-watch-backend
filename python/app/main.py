c65b09ddeb5e8ae0eaa2a2d6be45648275e82beb
kjlb <kjlb@localhost.localdomain>
2023-08-01 19:57:11 +0900

    refactor:

--- python/app/main.py
+++ python/app/main.py
@@ -1,6 +1,6 @@
 import datetime
 import upload
-import pie_chart
+import chart
 import data
 import schedule
 import time
@@ -17,12 +17,16 @@ def task():
 
     # 今日が月の最初の日かどうかを判定
     if today == first_day:
-        print("今日は月の最初の日です")
         log_times = data.get_log_times()
-        print(log_times)
-        image_path = pie_chart.save_users_log_time_pie_chart(log_times)
-        upload.post_slack(image_path)
 
+        sorted_log_times=sorted(
+            log_times.items(),key=lambda x:x[1],reverse=True
+        )
+        first_three_keys = [t[0] for t in sorted_log_times[:3]]
+        message=f'滞在時間ランキング\n1位 {first_three_keys[0]}さん\n2位 {first_three_keys[1]}さん\n3位 {first_three_keys[2]}さん\nおめでとうございます!'
+
+        image_path = chart.save_users_log_time_bar_chart(log_times,message)
+        upload.post_slack(image_path,message) 
 
 # 一日一回実行
 schedule.every(1).days.do(task)
---
+++ python/app/main_practice.py
@@ -0,0 +1,21 @@
+
+import datetime
+import upload
+import chart
+import data
+import schedule
+import time
+
+
+if __name__ == '__main__':
+    log_times=data.get_log_times() 
+
+    sorted_log_times=sorted(
+        log_times.items(),key=lambda x:x[1],reverse=True
+    )
+    first_three_keys = [t[0] for t in sorted_log_times[:3]]
+
+    message=f'滞在時間ランキング\n1位 {first_three_keys[0]}さん\n2位 {first_three_keys[1]}さん\n3位 {first_three_keys[2]}さん\nおめでとうございます!'
+    image_path=chart.save_users_log_time_bar_chart(log_times)
+    upload.post_slack(image_path,message)
+
