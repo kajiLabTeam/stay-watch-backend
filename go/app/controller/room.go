d548cb05d5ec70583dcb596fc91a7f38e74a9269
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-03-24 17:44:04 +0900

    fix: 部屋名が取得できない場合、roomName=削除済みになるように

--- go/app/controller/room.go
+++ go/app/controller/room.go
@@ -209,6 +209,7 @@ func GetLogs(c *gin.Context) {
 		roomName, err := RoomService.GetRoomNameByRoomID(log.RoomID)
 		if err != nil {
 			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "Failed to get room name"})
+			roomName = "削除済み"
 			return
 		}
 
