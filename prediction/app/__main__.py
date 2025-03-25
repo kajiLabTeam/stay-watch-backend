a0baba737de692d57873bc5c6fd690fdefee6256
Bayashin <kouhei1402@icloud.com>
2025-03-25 11:58:11 +0900

    feat:イベント発生時刻の予測

--- prediction/app/__main__.py
+++ prediction/app/__main__.py
@@ -1,11 +1,12 @@
 import uvicorn
-from app.api.endpoints import probability
+from app.api.endpoints import probability, time
 from fastapi import FastAPI
 
 
 def main():
     app = FastAPI()
     app.include_router(probability.router)
+    app.include_router(time.router)
     uvicorn.run(app, host="0.0.0.0", port=8085)
 
 
--- prediction/app/api/schemas.py
+++ prediction/app/api/schemas.py
@@ -4,6 +4,8 @@ from pydantic import BaseModel
 class ProbabilityResponse(BaseModel):
     probability: float
 
+class TimeResponse(BaseModel):
+    time: str
 
 class ClusteringResult(BaseModel):
     data: list[float]
